0c8c1fc69135dbe5b2c48f5b3599a765fbdf5423
Wynd-Hoply <wyndhoply@gmail.com>
2026-07-16 11:29:38 -0300

    fix(mobile): Ajuste na posição dos content dentro dos Div/Section, e na página como um todo para mobile

--- personal-landingpage/script.js
+++ personal-landingpage/script.js
@@ -18,3 +18,40 @@ window.addEventListener("scroll", () => {
     body.classList.remove("scrolled");
   }
 });
+
+
+window.addEventListener("scroll", () => {
+
+    if (window.innerWidth <= 900) {
+        header.classList.remove("scrolled");
+        return;
+    }
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+
+});
+
+
+document.querySelectorAll('.nav-menu a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const destino = document.querySelector(this.getAttribute('href'));
+
+    destino.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
+/* Eu sei que eu poderia fazer direto no CSS. Mas eu prefiro desse jeito pela velocidade. 
+
+html {
+    scroll-behavior: smooth;
+}
+
+*/
